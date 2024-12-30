272c50dcda23529fa566765d7b9486ea13e0dce2
fasihhhh <fasihrehman151@gmail.com>
2024-12-30 21:34:07 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// let input = document.getElementById('input')
+// let btn = document.getElementById('button')
+// let result = document.getElementsByClassName('result')[0]
+
+// let res = ""
+// let DisplayResult = (res) =>
+// {
+//     result.innerHTML = `${res}`
+// }
+// let primeCheck = () =>
+// {
+//     let sum =0;
+//     num = parseInt(input.value)
+//     if (isNaN(num) || num < 2) 
+//     {
+//         DisplayResult("Please enter a valid NUMBER greater than 1");
+//         return;
+//     }
+//     for (let i =2 ; i<num ; i++) {
+//         if(num%i==0){
+//             sum += i;
+//         }
+//     }
+//     console.log(sum)
+//     if(sum==0)
+//     {
+//         res = `"${num}"` +"  IS PRIME A NUMBER"
+//         DisplayResult(res)
+//     }
+//     if(sum>0)
+//     {
+//         res = `"${num}"` + "  IS NOT A PRIME NUMBER"
+//         DisplayResult(res)
+//     }
+// }
+// btn.addEventListener('click' , primeCheck)
+console.log("hellow")
